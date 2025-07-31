458d6ce9b5b68456c1b9313c40eac3ff91b63706
Juan Pirela <pirelajuan13@gmail.com>
2025-07-31 13:54:33 -0400

    Added authorization and ARCJET for rate limiting

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import subscriptionRouter from "./routes/subscriptions.routes.js";
 import connectDataBase from "./database/mongodb.js";
 
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/acrjet.middleware.js";
 
 
 
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
