948d3bbf260f4ffa051cd90289dad3b7a64689f3
Juan Pirela <pirelajuan13@gmail.com>
2025-07-31 11:42:01 -0400

    Added authentication controller

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {PORT} from "./config/env.js"
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
@@ -7,12 +8,21 @@ import subscriptionRouter from "./routes/subscriptions.routes.js";
 
 import connectDataBase from "./database/mongodb.js";
 
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send("Welcome to the Subscription Tracker API!")
